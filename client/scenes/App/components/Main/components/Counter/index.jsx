8caf572ad95ed5f2a772214f365db72253450556
Marios Fakiolas <m.fakiolas@hotmail.gr>
2017-04-26 17:44:52 +0300

    upgrade to react 15.5

--- client/scenes/App/components/Main/components/Counter/index.jsx
+++ client/scenes/App/components/Main/components/Counter/index.jsx
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import _padStart from 'lodash/padStart';
 
 import Button from 'components/Button';
--- server/index.js
+++ server/index.js
@@ -3,10 +3,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const compression = require('compression');
 const fallback = require('express-history-api-fallback');
+const webpack = require('webpack');
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
 
+const config = require('../webpack.config');
 const laps = require('./routes/laps');
 
-const { NODE_ENV } = process.env;
 const app = express();
 
 app.use(compression());
@@ -15,35 +18,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api/laps', laps);
 
-if (NODE_ENV === 'development') {
-  const webpack = require('webpack');
-  const webpackMiddleware = require('webpack-dev-middleware');
-  const webpackHotMiddleware = require('webpack-hot-middleware');
-  const config = require('../webpack.config');
-  const compiler = webpack(config);
+const compiler = webpack(config);
 
-  app.use(webpackHotMiddleware(compiler));
-  app.use(webpackMiddleware(compiler, {
-    publicPath: config.output.publicPath,
-    stats: { colors: true },
-  }));
+app.use(webpackHotMiddleware(compiler));
+app.use(webpackMiddleware(compiler, {
+  publicPath: config.output.publicPath,
+  stats: { colors: true },
+}));
 
-  app.use('*', (req, res, next) => {
-    const filename = join(compiler.outputPath, 'index.html');
-    compiler.outputFileSystem.readFile(filename, (err, result) => {
-      if (err) return next(err);
+app.use('*', (req, res, next) => {
+  const filename = join(compiler.outputPath, 'index.html');
+  compiler.outputFileSystem.readFile(filename, (err, result) => {
+    if (err) return next(err);
 
-      res.set('content-type', 'text/html');
-      res.send(result);
-      res.end();
-    });
+    res.set('content-type', 'text/html');
+    res.send(result);
+    res.end();
   });
-} else {
-  const root = join(__dirname, '../dist');
-
-  app.use(express.static(root));
-  app.use(fallback('index.html', { root }));
-}
+});
 
 // START THE SERVER
 app.listen(3000, () => console.log('Server started on port 3000'));
